0271f33f42cd67c62d584ad30261a0859106cd27
Santiago De Olivera <santiagodeolivera@gmail.com>
2021-10-23 08:33:27 -0300

    Add example code in Price constructor

--- src/Library/HighLevel/Accountability/Price.cs
+++ src/Library/HighLevel/Accountability/Price.cs
@@ -24,9 +24,15 @@ namespace Library.HighLevel.Accountability
         /// <summary>
         /// Creates an instance of <see cref="Price" />.
         /// </summary>
-        /// <param name="quantity"></param>
-        /// <param name="currency"></param>
-        /// <param name="unit"></param>
+        /// <param name="quantity">The numeric value.</param>
+        /// <param name="currency">The currency with which the price is determined.</param>
+        /// <param name="unit">The unit of material measurement with which the price is determined.</param>
+        /// <example>
+        ///     This example creates an instance of <see cref="Price" /> which represents U$S 30/kg;
+        ///     <code>
+        ///         new Price(30, Currency.Dollar, Unit.GetByAbbr("kg"));
+        ///     </code>
+        /// </example>
         public Price(float quantity, Currency currency, Unit unit)
         {
             this.Quantity = quantity;
