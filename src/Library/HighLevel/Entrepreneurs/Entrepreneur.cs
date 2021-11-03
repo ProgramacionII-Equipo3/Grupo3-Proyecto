1809c5893054a37f7c72ddfe112707fb53fa1ee1
Juan Nocetti <juan.anocetti@gmail.com>
2021-11-02 22:51:10 -0300

    problem solved

--- src/Library/HighLevel/Entrepreneurs/Entrepreneur.cs
+++ src/Library/HighLevel/Entrepreneurs/Entrepreneur.cs
@@ -66,7 +66,7 @@ namespace Library.HighLevel.Entrepreneurs
             this.Id = id;
             this.Name = name;
             this.Age = age;
-            this.Location = location;
+            this.Location = Location;
             this.Heading = heading;
         }
     }
