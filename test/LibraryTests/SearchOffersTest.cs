5ff3f58aaecb30ce1cc5982f550f0dc37534c29e
Bianca Luzzatto <bianca.luzzatto@correo.ucu.edu.uy>
2021-11-03 23:05:47 -0300

    test fixes

--- test/LibraryTests/SearchOffersTest.cs
+++ test/LibraryTests/SearchOffersTest.cs
@@ -31,35 +31,30 @@ namespace ProgramTests
         public void SearchOffersbyCategoryFound()
         {
             MaterialCategory category1 = new MaterialCategory("Residuos hospitalarios"); 
-            List<string> keyword1 = new List<string>();
-            keyword1.Add("agujas");
-            keyword1.Add("hospital");
+            List<string> keyword1 = new List<string>{ "agujas", "hospital" };
             Material material1 = Material.CreateInstance("Agujas Quirúrgicas", Measure.Weight, category1);
             Unit unit1 = new Unit("kilogram", "kg", 20, Measure.Weight);
             Amount amount1 = new Amount(100, unit1);
             Price price1 = new Price(1000, Currency.Peso, unit1);
             LocationApiClient client = new LocationApiClient();
-            Location pickupLocation1 = new Location();
-            pickupLocation1 = client.GetLocationAsync("Libertad 2500").Result;
+            Location pickupLocation1 = client.GetLocationAsync("Libertad 2500").Result;
             MaterialPublication publication1 = MaterialPublication.CreateInstance(material1, amount1, price1, pickupLocation1, keyword1);
-            MaterialPublication.AddPublication(publication1);
+            
 
             MaterialCategory category2 = new MaterialCategory("Residuos hospitalarios"); 
-            List<string> keyword2 = new List<string>();
-            keyword2.Add("hospital");
-            keyword2.Add("cubrebocas");
+            List<string> keyword2 = new List<string>{ "hospital", "cubrebocas" };
             Material material2 = Material.CreateInstance("Tapabocas Descartable", Measure.Weight, category2);
             Unit unit2 = new Unit("kilogram", "kg", 5, Measure.Weight);
             Amount amount2 = new Amount(500, unit2);
             Price price2 = new Price(800, Currency.Peso, unit2);
-            Location pickupLocation2 = new Location();
-            pickupLocation2 = client.GetLocationAsync("Dr. Gustavo Gallinal 1720").Result;
+            Location pickupLocation2 = client.GetLocationAsync("Dr. Gustavo Gallinal 1720").Result;
             MaterialPublication publication2 = MaterialPublication.CreateInstance(material2, amount2, price2, pickupLocation2, keyword2);
-            MaterialPublication.AddPublication(publication2);
+           
+            List<MaterialPublication> publicationsToSearchIn = new List<MaterialPublication>{ publication1, publication2 };
 
-            MaterialCategory category = new MaterialCategory("Residuos hospitalarios");
+            MaterialCategory categoryToSearch = new MaterialCategory("Residuos hospitalarios");
             SearchByCategory searchByCategory = new SearchByCategory();
-            searchByCategory.Search(MaterialPublication.publications, category);
+            searchByCategory.Search(publicationsToSearchIn, categoryToSearch);
 
             List<MaterialPublication> expected1 = new List<MaterialPublication>();
             expected1.Add(publication1);
@@ -77,41 +72,36 @@ namespace ProgramTests
         public void SearchOffersbyCategoryNotFound()
         {
             MaterialCategory category1 = new MaterialCategory("Residuos hospitalarios"); 
-            List<string> keyword1 = new List<string>();
-            keyword1.Add("agujas");
-            keyword1.Add("hospital");
+            List<string> keyword1 = new List<string>{ "agujas", "hospital" };
             Material material1 = Material.CreateInstance("Agujas Quirúrgicas", Measure.Weight, category1);
             Unit unit1 = new Unit("kilogram", "kg", 20, Measure.Weight);
             Amount amount1 = new Amount(100, unit1);
             Price price1 = new Price(1000, Currency.Peso, unit1);
             LocationApiClient client = new LocationApiClient();
-            Location pickupLocation1 = new Location();
-            pickupLocation1 = client.GetLocationAsync("Libertad 2500").Result;
+            Location pickupLocation1 = client.GetLocationAsync("Libertad 2500").Result;
             MaterialPublication publication1 = MaterialPublication.CreateInstance(material1, amount1, price1, pickupLocation1, keyword1);
-            MaterialPublication.AddPublication(publication1);
+ 
 
             MaterialCategory category2 = new MaterialCategory("Residuos hospitalarios"); 
-            List<string> keyword2 = new List<string>();
-            keyword2.Add("hospital");
-            keyword2.Add("cubrebocas");
+            List<string> keyword2 = new List<string>{ "hospital", "cubrebocas" };
             Material material2 = Material.CreateInstance("Tapabocas Descartable", Measure.Weight, category2);
             Unit unit2 = new Unit("kilogram", "kg", 5, Measure.Weight);
             Amount amount2 = new Amount(500, unit2);
             Price price2 = new Price(800, Currency.Peso, unit2);
-            Location pickupLocation2 = new Location();
-            pickupLocation2 = client.GetLocationAsync("Dr. Gustavo Gallinal 1720").Result;
+            Location pickupLocation2 = client.GetLocationAsync("Dr. Gustavo Gallinal 1720").Result;
             MaterialPublication publication2 = MaterialPublication.CreateInstance(material2, amount2, price2, pickupLocation2, keyword2);
-            MaterialPublication.AddPublication(publication2);
 
-            MaterialCategory Category = new MaterialCategory("Materia Prima");
-            SearchByCategory searchByCategoryTest = new SearchByCategory();
+            List<MaterialPublication> publicationsToSearchIn = new List<MaterialPublication>{ publication1, publication2 };
+
+            MaterialCategory categoryToSearch = new MaterialCategory("Materia Prima");
+            SearchByCategory searchByCategory = new SearchByCategory();
             SearchByCategory.categorySearcher.Clear();
-            searchByCategoryTest.Search(MaterialPublication.publications, Category);
+            searchByCategory.Search(publicationsToSearchIn, categoryToSearch);
 
             List<MaterialPublication> expected2 = new List<MaterialPublication>();
 
             Assert.AreEqual(SearchByCategory.categorySearcher, expected2);
-        }/*
+        }
 
         /// <summary>
         /// This test checks that an entrepreneur is able to
@@ -120,36 +110,29 @@ namespace ProgramTests
         [Test]
         public void SearchOffersbyKeywordsFound()
         {
-                        MaterialCategory category1 = new MaterialCategory("Residuos hospitalarios"); 
-            List<string> keyword1 = new List<string>();
-            keyword1.Add("agujas");
-            keyword1.Add("hospital");
+            MaterialCategory category1 = new MaterialCategory("Residuos hospitalarios"); 
+            List<string> keyword1 = new List<string>{ "agujas", "hospital" };
             Material material1 = Material.CreateInstance("Agujas Quirúrgicas", Measure.Weight, category1);
             Unit unit1 = new Unit("kilogram", "kg", 20, Measure.Weight);
             Amount amount1 = new Amount(100, unit1);
             Price price1 = new Price(1000, Currency.Peso, unit1);
             LocationApiClient client = new LocationApiClient();
-            Location pickupLocation1 = new Location();
-            pickupLocation1 = client.GetLocationAsync("Libertad 2500").Result;
+            Location pickupLocation1 = client.GetLocationAsync("Libertad 2500").Result;
             MaterialPublication publication1 = MaterialPublication.CreateInstance(material1, amount1, price1, pickupLocation1, keyword1);
-            MaterialPublication.AddPublication(publication1);
 
             MaterialCategory category2 = new MaterialCategory("Residuos hospitalarios"); 
-            List<string> keyword2 = new List<string>();
-            keyword2.Add("hospital");
-            keyword2.Add("cubrebocas");
+            List<string> keyword2 = new List<string>{ "hospital", "cubrebocas" };
             Material material2 = Material.CreateInstance("Tapabocas Descartable", Measure.Weight, category2);
             Unit unit2 = new Unit("kilogram", "kg", 5, Measure.Weight);
             Amount amount2 = new Amount(500, unit2);
             Price price2 = new Price(800, Currency.Peso, unit2);
-            Location pickupLocation2 = new Location();
-            pickupLocation2 = client.GetLocationAsync("Dr. Gustavo Gallinal 1720").Result;
+            Location pickupLocation2 = client.GetLocationAsync("Dr. Gustavo Gallinal 1720").Result;
             MaterialPublication publication2 = MaterialPublication.CreateInstance(material2, amount2, price2, pickupLocation2, keyword2);
-            MaterialPublication.AddPublication(publication2);
 
-            SearchByKeyword searchByKeywordTest = new SearchByKeyword();
-            SearchByKeyword.keywordSearcher.Clear();
-            searchByKeywordTest.Search(MaterialPublication.publications, "cubrebocas");
+            List<MaterialPublication> publicationsToSearchIn = new List<MaterialPublication>{ publication1, publication2 };
+
+            SearchByKeyword searchByKeyword = new SearchByKeyword();
+            searchByKeyword.Search(publicationsToSearchIn, "cubrebocas");
 
             List<MaterialPublication> expected3 = new List<MaterialPublication>();
             expected3.Add(publication2);
@@ -165,13 +148,35 @@ namespace ProgramTests
         [Test]
         public void SearchOffersbyKeywordsNotFound()
         {
+            MaterialCategory category1 = new MaterialCategory("Residuos hospitalarios"); 
+            List<string> keyword1 = new List<string>{ "agujas", "hospital" };
+            Material material1 = Material.CreateInstance("Agujas Quirúrgicas", Measure.Weight, category1);
+            Unit unit1 = new Unit("kilogram", "kg", 20, Measure.Weight);
+            Amount amount1 = new Amount(100, unit1);
+            Price price1 = new Price(1000, Currency.Peso, unit1);
+            LocationApiClient client = new LocationApiClient();
+            Location pickupLocation1 = client.GetLocationAsync("Libertad 2500").Result;
+            MaterialPublication publication1 = MaterialPublication.CreateInstance(material1, amount1, price1, pickupLocation1, keyword1);
+
+            MaterialCategory category2 = new MaterialCategory("Residuos hospitalarios"); 
+            List<string> keyword2 = new List<string>{ "hospital", "cubrebocas" };
+            Material material2 = Material.CreateInstance("Tapabocas Descartable", Measure.Weight, category2);
+            Unit unit2 = new Unit("kilogram", "kg", 5, Measure.Weight);
+            Amount amount2 = new Amount(500, unit2);
+            Price price2 = new Price(800, Currency.Peso, unit2);
+            Location pickupLocation2 = client.GetLocationAsync("Dr. Gustavo Gallinal 1720").Result;
+            MaterialPublication publication2 = MaterialPublication.CreateInstance(material2, amount2, price2, pickupLocation2, keyword2);
+
+            List<MaterialPublication> publicationsToSearchIn = new List<MaterialPublication>{ publication1, publication2 };
+
             SearchByKeyword searchByKeyword = new SearchByKeyword();
-            searchByKeyword.Search(MaterialPublication.publications, "sanitario");
+            SearchByKeyword.keywordSearcher.Clear();
+            searchByKeyword.Search(publicationsToSearchIn, "sanitario");
 
             List<MaterialPublication> expected4 = new List<MaterialPublication>();
 
             Assert.AreEqual(SearchByKeyword.keywordSearcher, expected4);
-        }
+        }/*
 
         /// <summary>
         /// This test checks that an entrepreneur is able to
@@ -180,12 +185,33 @@ namespace ProgramTests
         [Test]
         public void SearchOffersbyZoneFound()
         {
+            MaterialCategory category1 = new MaterialCategory("Residuos hospitalarios"); 
+            List<string> keyword1 = new List<string>{ "agujas", "hospital" };
+            Material material1 = Material.CreateInstance("Agujas Quirúrgicas", Measure.Weight, category1);
+            Unit unit1 = new Unit("kilogram", "kg", 20, Measure.Weight);
+            Amount amount1 = new Amount(100, unit1);
+            Price price1 = new Price(1000, Currency.Peso, unit1);
+            LocationApiClient client = new LocationApiClient();
+            Location pickupLocation1 = client.GetLocationAsync("Libertad 2500").Result;
+            MaterialPublication publication1 = MaterialPublication.CreateInstance(material1, amount1, price1, pickupLocation1, keyword1);
+
+            MaterialCategory category2 = new MaterialCategory("Residuos hospitalarios"); 
+            List<string> keyword2 = new List<string>{ "hospital", "cubrebocas" };
+            Material material2 = Material.CreateInstance("Tapabocas Descartable", Measure.Weight, category2);
+            Unit unit2 = new Unit("kilogram", "kg", 5, Measure.Weight);
+            Amount amount2 = new Amount(500, unit2);
+            Price price2 = new Price(800, Currency.Peso, unit2);
+            Location pickupLocation2 = client.GetLocationAsync("Dr. Gustavo Gallinal 1720").Result;
+            MaterialPublication publication2 = MaterialPublication.CreateInstance(material2, amount2, price2, pickupLocation2, keyword2);
+
+            List<MaterialPublication> publicationsToSearchIn = new List<MaterialPublication>{ publication1, publication2 };
+
             SearchByLocation searchByLocation = new SearchByLocation();
             LocationApiClient clientTest = new LocationApiClient();
             Location locationSpecified = new Location();
             locationSpecified = clientTest.GetLocationAsync("Av. Gral. San Martín 2909").Result;
             double distanceSpecified = 100;
-            searchByLocation.Search(MaterialPublication.publications, locationSpecified, distanceSpecified);
+            searchByLocation.Search(publicationsToSearchIn, locationSpecified, distanceSpecified);
 
             List<MaterialPublication> expected5 = new List<MaterialPublication>();
             expected5.Add(publication2);
