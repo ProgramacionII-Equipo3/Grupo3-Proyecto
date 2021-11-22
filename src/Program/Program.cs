c023b38daf4bb3f678586e491b067274dd874ea5
Santiago De Olivera <santiagodeolivera@gmail.com>
2021-11-22 16:45:50 -0300

    Finish testing deserialization

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -23,25 +23,10 @@ namespace Grupo3_Proyecto
         /// </summary>
         public static void Main(string[] args)
         {
-            if(args.Length != 1) throw new Exception("One command-line argument expected.");
-            switch(args[0].ToLower())
-            {
-                case "--admin":
-                    Singleton<SessionManager>.Instance.NewUser("ConsoleID", new UserData("console user", true, UserData.Type.ADMIN, null, null), new Library.States.Admins.AdminInitialMenuState());
-                    break;
-                case "--entrepreneur":
-                    break;
-                case "--company":
-                    Singleton<InvitationManager>.Instance.CreateInvitation<Library.HighLevel.Companies.CompanyInvitation>("4jsk", code => new Library.HighLevel.Companies.CompanyInvitation(code));
-                    break;
-                case "--json":
-                    Library.Utils.SerializationUtils.DeserializeAllFromJSON("../../Memory");
-                    break;
-                default:
-                    throw new Exception("Command-line argument expected to be \"--admin\", \"--entrepreneur\", \"--company\", or \"--JSON\"");
-            }
+            Library.Utils.SerializationUtils.DeserializeAllFromJSON("../../Memory");
             Console.WriteLine("----------");
-            new ConsolePlatform().Run();
+            new MultipleUsersConsolePlatform("ConsoleID").Run();
+//            Library.Utils.SerializationUtils.SerializeAllIntoJSON("../../Memory");
         }
     }
 }
