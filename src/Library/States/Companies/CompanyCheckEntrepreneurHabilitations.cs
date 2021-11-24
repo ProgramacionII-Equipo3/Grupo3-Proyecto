765d500f5c895837da767c8d594fd7703097bb9d
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2021-11-23 22:09:01 -0300

    CheckHabilitations command implemented

--- src/Library/States/Companies/CompanyCheckEntrepreneurHabilitations.cs
+++ src/Library/States/Companies/CompanyCheckEntrepreneurHabilitations.cs
@@ -1,9 +1,31 @@
-/* using Library.Core;
+using System;
+using System.Collections.Generic;
+using Library.Core;
+using Library.Core.Processing;
+using Library.HighLevel.Entrepreneurs;
+using Library.HighLevel.Materials;
+using Library.InputHandlers;
+using Library.States;
 
 namespace Library.States.Companies
 {
-    public class CompanyCheckEntrepreneurHabilitations : InputHandlerState
+    public class CompanyCheckEntrepreneurHabilitations : WrapperState
     {
-       
+        public CompanyCheckEntrepreneurHabilitations(string id) : base(
+            InputProcessorState.CreateInstance<string>(
+                new BasicStringProcessor(() => "Inserte el nombre del emprendedor que desea chequear."),
+                entrepreneur =>
+                {
+                    if (Singleton<EntrepreneurManager>.Instance.GetByName(entrepreneur) is Entrepreneur entrepreneurResult)
+                    {
+                        IList<Habilitation> habilitationsResult = entrepreneurResult.Habilitations;
+                        return (new CompanyInitialMenuState(string.Join("\n", habilitationsResult)), null);
+                    }
+                },
+                () => (new CompanyInitialMenuState(id), null)
+            )
+        )
+        {
+        }
     }
-} */
+}
