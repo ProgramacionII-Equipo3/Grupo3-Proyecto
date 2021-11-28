8a712eaeaf20252a5d156d6fb8cc109152b3b528
Santiago De Olivera <santiagodeolivera@gmail.com>
2021-11-28 10:12:29 -0300

    Correct small warning in class JsonMaterialPublication

--- src/Library/HighLevel/Materials/JsonMaterialPublication.cs
+++ src/Library/HighLevel/Materials/JsonMaterialPublication.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text.Json.Serialization;
 using System.Linq;
-using System.Text.Json.Serialization;
 using Library.HighLevel.Accountability;
 using Ucu.Poo.Locations.Client;
 using Library.Utils;
